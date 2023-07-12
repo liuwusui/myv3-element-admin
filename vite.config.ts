ed75ceeb9d979f542d7a43587ff5e8ac6ddc81ec
刘五岁叻 <2037525136@qq.com>
2023-07-12 10:33:10 +0800

    反向代理解决跨域

--- vite.config.ts
+++ vite.config.ts
@@ -55,21 +55,21 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
         }
       }
     },
-    // server: {
-    //   host: '0.0.0.0',
-    //   port: Number(env.VITE_APP_PORT),
-    //   open: true, // 运行是否自动打开浏览器
-    //   // proxy: {
-    //   //   // 反向代理解决跨域
-    //   //   [env.VITE_APP_BASE_API]: {
-    //   //     target: 'http://vapi.youlai.tech', // 线上接口地址
-    //   //     // target: 'http://localhost:8989',  // 本地接口地址 , 后端工程仓库地址：https://gitee.com/youlaiorg/youlai-boot
-    //   //     changeOrigin: true,
-    //   //     rewrite: (path) =>
-    //   //       path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '') // 替换 /dev-api 为 target 接口地址
-    //   //   }
-    //   // }
-    // },
+    server: {
+      host: '0.0.0.0',
+      port: Number(env.VITE_APP_PORT),
+      open: true, // 运行是否自动打开浏览器
+      proxy: {
+        // 反向代理解决跨域
+        [env.VITE_APP_BASE_API]: {
+          target: 'http://vapi.youlai.tech', // 线上接口地址
+          // target: 'http://localhost:8989',  // 本地接口地址 , 后端工程仓库地址：https://gitee.com/youlaiorg/youlai-boot
+          changeOrigin: true,
+          rewrite: (path) =>
+            path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '') // 替换 /dev-api 为 target 接口地址
+        }
+      }
+    },
     plugins: [
       vue(),
       AutoImport({
